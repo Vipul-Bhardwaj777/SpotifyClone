0923c73da8b50f3be3768a6b963779d9b661321c
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2024-02-12 16:29:22 +0530

    Revert commits up to abcd123

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import {View, Text, SafeAreaView, ActivityIndicator} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import {addTrack, setupPlayer} from '../musicPlayerService';
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const App = () => {
-  const [isPlayerReady, setIsPlayerReady] = useState(false);
-
-  async function setup() {
-    let isSetup = await setupPlayer();
-
-    if (isSetup) {
-      await addTrack();
-    }
-    setIsPlayerReady(isSetup);
-  }
-
-  useEffect(() => {
-    setup();
-  }, []);
-
-  return !isPlayerReady ? (
-    <SafeAreaView
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <ActivityIndicator />
-    </SafeAreaView>
-  ) : (
+  return (
     <View>
       <Text>App</Text>
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
