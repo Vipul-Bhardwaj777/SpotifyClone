15b1e1ba402ecd288f155fca6557274ca4f4c668
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2024-02-12 13:18:33 +0530

    feat: created controlcenter, songinfo, songslider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, SafeAreaView, ActivityIndicator} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {addTrack, setupPlayer} from '../musicPlayerService';
 
 const App = () => {
-  return (
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  async function setup() {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTrack();
+    }
+    setIsPlayerReady(isSetup);
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  return !isPlayerReady ? (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <ActivityIndicator />
+    </SafeAreaView>
+  ) : (
     <View>
       <Text>App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
